3f220c28fd9a95d2d3befae4c3ace0561692e305
Iván López <lopez.ivan@gmail.com>
2018-01-16 13:28:05 +0100

    Add tests for courseSubscriber domain class constraints

--- app/grails-app/domain/org/grails/onezeroone/repository/gorm/CourseSubscriberGormEntity.groovy
+++ app/grails-app/domain/org/grails/onezeroone/repository/gorm/CourseSubscriberGormEntity.groovy
@@ -1,7 +1,6 @@
 package org.grails.onezeroone.repository.gorm
 
 import grails.compiler.GrailsCompileStatic
-import org.grails.onezeroone.CourseSubscriber
 import org.grails.onezeroone.SubscriptionDay
 
 @GrailsCompileStatic
@@ -11,7 +10,7 @@ class CourseSubscriberGormEntity {
 
     static constraints = {
         email nullable: false, blank: false, email: true, unique: true
-
+        day nullable: false
     }
     static mapping = {
         table 'coursesubscriber'
