e9a441aa5531962439b83bd4a1525fbbbb68addb
Iván López <lopez.ivan@gmail.com>
2018-01-17 11:47:34 +0100

    UseCase for course subscription

---
+++ app/grails-app/services/org/grails/onezeroone/usecase/SubscribeUseCaseService.groovy
@@ -0,0 +1,23 @@
+package org.grails.onezeroone.usecase
+
+import grails.validation.ValidationException
+import groovy.transform.CompileStatic
+import org.grails.onezeroone.CourseSubscriber
+import org.grails.onezeroone.CourseSubscriberRepository
+import org.grails.onezeroone.SubscriptionDay
+import org.grails.onezeroone.entities.CourseSubscriberImpl
+
+@CompileStatic
+class SubscribeUseCaseService {
+    CourseSubscriberRepository courseSubscriberRepository
+
+    void subscribe(String email) {
+        CourseSubscriber courseSubscriber = new CourseSubscriberImpl(email: email, subscriptionDay: SubscriptionDay.ONE)
+
+        try {
+            courseSubscriberRepository.save(courseSubscriber)
+        } catch (ValidationException ve) {
+            log.warn "There was an error saving the subscriber: ${ve.message}"
+        }
+    }
+}
