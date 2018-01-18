7a787aec35e3502345a77711464b23bcd4e18d33
Sergio del Amo <sergio.delamo@softamo.com>
2018-01-18 09:41:22 +0100

    Remove wrong import

--- app/src/test/groovy/org/grails/onezeroone/plugins/ui/controllers/UrlMappingsSpec.groovy
+++ app/src/test/groovy/org/grails/onezeroone/plugins/ui/controllers/UrlMappingsSpec.groovy
@@ -1,7 +1,6 @@
 package org.grails.onezeroone.plugins.ui.controllers
 
 import grails.testing.web.UrlMappingsUnitTest
-import grails.web.mapping.UrlMappings
 import spock.lang.Specification
 
 class UrlMappingsSpec extends Specification implements UrlMappingsUnitTest<UrlMappings> {
