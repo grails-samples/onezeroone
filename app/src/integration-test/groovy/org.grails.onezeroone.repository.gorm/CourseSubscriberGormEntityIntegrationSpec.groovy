494476b1730191f1fda495fde9160ee9c455641f
Sergio del Amo <sergio.delamo@softamo.com>
2018-01-18 09:13:19 +0100

    Remove spock indentation from integration tests

--- app/src/integration-test/groovy/org.grails.onezeroone.repository.gorm/CourseSubscriberGormEntityIntegrationSpec.groovy
+++ app/src/integration-test/groovy/org.grails.onezeroone.repository.gorm/CourseSubscriberGormEntityIntegrationSpec.groovy
@@ -13,55 +13,55 @@ class CourseSubscriberGormEntityIntegrationSpec extends HibernateSpec {
     @Unroll
     void "email '#value' #description"() {
         when:
-            def courseSubscriber = new CourseSubscriberGormEntity(email: value)
+        def courseSubscriber = new CourseSubscriberGormEntity(email: value)
 
         then:
-            courseSubscriber.validate(['email']) == expected
-            courseSubscriber.errors['email']?.code == expectedErrorCode
+        courseSubscriber.validate(['email']) == expected
+        courseSubscriber.errors['email']?.code == expectedErrorCode
 
         where:
-            value              | expected | expectedErrorCode
-            null               | false    | 'nullable'
-            ''                 | false    | 'nullable' // because of conversion empty String to null
-            'not-email'        | false    | 'email.invalid'
-            'john@example.com' | true     | null
-            description = expected ? 'is valid' : 'is not valid'
+        value              | expected | expectedErrorCode
+        null               | false    | 'nullable'
+        ''                 | false    | 'nullable' // because of conversion empty String to null
+        'not-email'        | false    | 'email.invalid'
+        'john@example.com' | true     | null
+        description = expected ? 'is valid' : 'is not valid'
     }
 
     @Unroll
     void "day '#value' #description"() {
         when:
-            def courseSubscriber = new CourseSubscriberGormEntity(day: value)
+        def courseSubscriber = new CourseSubscriberGormEntity(day: value)
 
         then:
-            courseSubscriber.validate(['day']) == expected
-            courseSubscriber.errors['day']?.code == expectedErrorCode
+        courseSubscriber.validate(['day']) == expected
+        courseSubscriber.errors['day']?.code == expectedErrorCode
 
         where:
-            value               | expected | expectedErrorCode
-            null                | false    | 'nullable'
-            SubscriptionDay.SIX | true     | null
-            description = expected ? 'is valid' : 'is not valid'
+        value               | expected | expectedErrorCode
+        null                | false    | 'nullable'
+        SubscriptionDay.SIX | true     | null
+        description = expected ? 'is valid' : 'is not valid'
     }
 
     void 'test email unique constraint'() {
         given: 'an existing courseSubscriber'
-            def courseSubscriber = new CourseSubscriberGormEntity(email: email, day: SubscriptionDay.FIVE).save()
-            assert !courseSubscriber.hasErrors()
+        def courseSubscriber = new CourseSubscriberGormEntity(email: email, day: SubscriptionDay.FIVE).save()
+        assert !courseSubscriber.hasErrors()
 
         when: 'trying to create another courseSubscriber with the same email'
-            def newCourseSubscriber = new CourseSubscriberGormEntity(email: email, day: SubscriptionDay.SEVEN)
-            newCourseSubscriber.save()
+        def newCourseSubscriber = new CourseSubscriberGormEntity(email: email, day: SubscriptionDay.SEVEN)
+        newCourseSubscriber.save()
 
         then: 'it is not created'
-            newCourseSubscriber.hasErrors()
-            newCourseSubscriber.errors['email'].code == 'unique'
+        newCourseSubscriber.hasErrors()
+        newCourseSubscriber.errors['email'].code == 'unique'
 
         and:
-            CourseSubscriberGormEntity.count() == 1
+        CourseSubscriberGormEntity.count() == 1
 
         where:
-            email = 'john@example.com'
+        email = 'john@example.com'
 
     }
 }
--- app/src/integration-test/groovy/org.grails.onezeroone.repository.gorm/CourseSubscriberGormServiceIntegrationSpec.groovy
+++ app/src/integration-test/groovy/org.grails.onezeroone.repository.gorm/CourseSubscriberGormServiceIntegrationSpec.groovy
@@ -15,53 +15,53 @@ class CourseSubscriberGormServiceIntegrationSpec extends Specification {
 
     void 'test find all courseSubscribers by day'() {
         given: 'a few courseSubscribers'
-            courseSubscriberDataService.save('user1A@example.com', SubscriptionDay.ONE)
-            courseSubscriberDataService.save('user1B@example.com', SubscriptionDay.ONE)
-            courseSubscriberDataService.save('user2A@example.com', SubscriptionDay.TWO)
-            courseSubscriberDataService.save('user3A@example.com', SubscriptionDay.THREE)
-            courseSubscriberDataService.save('user4A@example.com', SubscriptionDay.FOUR)
-            courseSubscriberDataService.save('user5A@example.com', SubscriptionDay.FIVE)
-            courseSubscriberDataService.save('user6A@example.com', SubscriptionDay.SIX)
-            courseSubscriberDataService.save('finished@example.com', SubscriptionDay.FINISHED)
+        courseSubscriberDataService.save('user1A@example.com', SubscriptionDay.ONE)
+        courseSubscriberDataService.save('user1B@example.com', SubscriptionDay.ONE)
+        courseSubscriberDataService.save('user2A@example.com', SubscriptionDay.TWO)
+        courseSubscriberDataService.save('user3A@example.com', SubscriptionDay.THREE)
+        courseSubscriberDataService.save('user4A@example.com', SubscriptionDay.FOUR)
+        courseSubscriberDataService.save('user5A@example.com', SubscriptionDay.FIVE)
+        courseSubscriberDataService.save('user6A@example.com', SubscriptionDay.SIX)
+        courseSubscriberDataService.save('finished@example.com', SubscriptionDay.FINISHED)
 
         when: 'finding all by day'
-            def result = courseSubscriberGormService.findAllByDay(SubscriptionDay.ONE)
+        def result = courseSubscriberGormService.findAllByDay(SubscriptionDay.ONE)
 
         then:
-            result != null
-            result.size() == 2
-            result.email == ['user1A@example.com', 'user1B@example.com']
-            result.subscriptionDay.every { it == SubscriptionDay.ONE }
+        result != null
+        result.size() == 2
+        result.email == ['user1A@example.com', 'user1B@example.com']
+        result.subscriptionDay.every { it == SubscriptionDay.ONE }
     }
 
     void 'move a list of courseSubscribers to another day'() {
         given: 'a few courseSubscribers'
-            def day1A = CourseSubscriberGormEntityUtils.of(courseSubscriberDataService.save('user1A@example.com', SubscriptionDay.ONE))
-            def day1B = CourseSubscriberGormEntityUtils.of(courseSubscriberDataService.save('user1B@example.com', SubscriptionDay.ONE))
-            CourseSubscriberGormEntityUtils.of(courseSubscriberDataService.save('user2A@example.com', SubscriptionDay.TWO))
-            CourseSubscriberGormEntityUtils.of(courseSubscriberDataService.save('user3A@example.com', SubscriptionDay.THREE))
+        def day1A = CourseSubscriberGormEntityUtils.of(courseSubscriberDataService.save('user1A@example.com', SubscriptionDay.ONE))
+        def day1B = CourseSubscriberGormEntityUtils.of(courseSubscriberDataService.save('user1B@example.com', SubscriptionDay.ONE))
+        CourseSubscriberGormEntityUtils.of(courseSubscriberDataService.save('user2A@example.com', SubscriptionDay.TWO))
+        CourseSubscriberGormEntityUtils.of(courseSubscriberDataService.save('user3A@example.com', SubscriptionDay.THREE))
 
         when: 'moving the courses from day ONE to day TWO'
-            courseSubscriberGormService.moveToDay([day1A, day1B], SubscriptionDay.TWO)
+        courseSubscriberGormService.moveToDay([day1A, day1B], SubscriptionDay.TWO)
 
         then:
-            courseSubscriberGormService.findAllByDay(SubscriptionDay.TWO).size() == 3
+        courseSubscriberGormService.findAllByDay(SubscriptionDay.TWO).size() == 3
     }
 
     void 'persist a courseSubscriber'() {
         given: 'a courseSubscriber'
-            def courseSubscriber = new CourseSubscriberImpl(email, subscriptionDay)
+        def courseSubscriber = new CourseSubscriberImpl(email, subscriptionDay)
 
         when: 'persisting it'
-            courseSubscriberGormService.save(courseSubscriber)
+        courseSubscriberGormService.save(courseSubscriber)
 
         then: 'it is persisted'
-            courseSubscriberGormService.findAllByDay(SubscriptionDay.FIVE).size() == 1
-            courseSubscriberGormService.findAllByDay(SubscriptionDay.FIVE).first().email == email
-            courseSubscriberGormService.findAllByDay(SubscriptionDay.FIVE).first().subscriptionDay == subscriptionDay
+        courseSubscriberGormService.findAllByDay(SubscriptionDay.FIVE).size() == 1
+        courseSubscriberGormService.findAllByDay(SubscriptionDay.FIVE).first().email == email
+        courseSubscriberGormService.findAllByDay(SubscriptionDay.FIVE).first().subscriptionDay == subscriptionDay
 
         where:
-            email = 'user1@example.com'
-            subscriptionDay = SubscriptionDay.FIVE
+        email = 'user1@example.com'
+        subscriptionDay = SubscriptionDay.FIVE
     }
 }
