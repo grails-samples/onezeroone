114392f765f6e27f42c1e6e9598e3c259a9ed592
Sergio del Amo <sergio.delamo@softamo.com>
2018-01-22 09:41:05 +0100

    add debug statements

--- app/grails-app/services/org/grails/onezeroone/usecase/DailyEmailUseCaseService.groovy
+++ app/grails-app/services/org/grails/onezeroone/usecase/DailyEmailUseCaseService.groovy
@@ -1,6 +1,7 @@
 package org.grails.onezeroone.usecase
 
 import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
 import org.grails.onezeroone.CourseSubscriber
 import org.grails.onezeroone.CourseSubscriberRepository
 import org.grails.onezeroone.Email
@@ -8,6 +9,7 @@ import org.grails.onezeroone.EmailComposer
 import org.grails.onezeroone.EmailService
 import org.grails.onezeroone.SubscriptionDay
 
+@Slf4j
 @CompileStatic
 class DailyEmailUseCaseService {
 
@@ -19,7 +21,13 @@ class DailyEmailUseCaseService {
         for (SubscriptionDay day : SubscriptionDay.values()) {
             if (!SubscriptionDay.hasFinished(day)) {
                 List<CourseSubscriber> subscribers = courseSubscriberRepository.findAllByDay(day)
+                if ( !subscribers ) {
+                    log.debug 'no {} email sent. No subscribers', day.name()
+                    continue
+                }
                 Email email = emailComposer.compose(day)
+
+                log.debug('sending {} email to {} subscribers', day.name(), subscribers.size())
 
                 emailService.send(subscribers, email)
                 courseSubscriberRepository.moveToDay(subscribers, SubscriptionDay.nextDay(day))
